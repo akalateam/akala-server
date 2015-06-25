ba71a1281a6c3c9abbe8475fd553f4dfa6f6393f
ksieee <ksieee@gmail.com>
2015-06-25 21:36:49 +0800

    Ensure Geo Index for Akala Shop
    Signed-off-by: ksieee <ksieee@gmail.com>

--- src/main/java/org/akala/server/shop/service/impl/AkalaShopServiceImpl.java
+++ src/main/java/org/akala/server/shop/service/impl/AkalaShopServiceImpl.java
@@ -3,6 +3,8 @@ package org.akala.server.shop.service.impl;
 import java.math.BigInteger;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.akala.server.shop.bean.AkalaShop;
 import org.akala.server.shop.bean.AkalaShopItem;
 import org.akala.server.shop.repository.AkalaShopRepository;
@@ -14,15 +16,29 @@ import org.springframework.data.geo.GeoResults;
 import org.springframework.data.geo.Metric;
 import org.springframework.data.geo.Metrics;
 import org.springframework.data.geo.Point;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
+import org.springframework.data.mongodb.core.index.GeospatialIndex;
 import org.springframework.stereotype.Service;
 
 @Service("akalaShopService")
 public class AkalaShopServiceImpl implements AkalaShopService {
-  
+
   @Autowired
   private AkalaShopRepository akalaShopRepository;
-  
-  public static Metric metric = new Metric() {    
+
+  @Autowired
+  private MongoTemplate template;
+
+  @PostConstruct
+  public void ensure2DIndex() {
+    template.indexOps(AkalaShop.class).ensureIndex(
+        new GeospatialIndex("coordinate").typed(GeoSpatialIndexType.GEO_2D));
+    template.indexOps(AkalaShop.class).ensureIndex(
+        new GeospatialIndex("coordinate").typed(GeoSpatialIndexType.GEO_2DSPHERE));
+  }
+
+  public static Metric metric = new Metric() {
     private static final long serialVersionUID = 1L;
 
     @Override
@@ -32,7 +48,7 @@ public class AkalaShopServiceImpl implements AkalaShopService {
   };
 
   @Override
-  public List<GeoResult<AkalaShop>> findAkalaShopList(float longitude, float latitude) {    
+  public List<GeoResult<AkalaShop>> findAkalaShopList(float longitude, float latitude) {
 //    return akalaShopRepository.findByCoordinateLongitudeAndCoordinateLatitude(longitude, latitude);
     
     Point point = new Point(longitude, latitude);
